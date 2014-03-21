c9c79a49a954b6f0313b37824a5faf36f7bb1f40
Florian Fesseler <ffesseler@kapit.fr>
2014-03-21 15:42:24 +0100

    Level 3 : Share

--- web.js
+++ web.js
@@ -1,11 +1,16 @@
 var express = require("express");
 var logfmt = require("logfmt");
+var jade = require('jade');
+var path = require('path');
 var app = express();
 
 app.use(logfmt.requestLogger());
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
 app.get('/', function(req, res) {
-    res.send('Hello Yose!');
+    res.render('index', { title: 'Express' });
 });
 
 app.get('/ping', function(req, res) {

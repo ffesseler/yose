7a56ceb20fe02f6f34d09a98aeb456f1e3a0b147
Florian Fesseler <ffesseler@kapit.fr>
2014-03-21 15:07:06 +0100

    Level 2 : First web service

--- web.js
+++ web.js
@@ -8,6 +8,12 @@ app.get('/', function(req, res) {
     res.send('Hello Yose!');
 });
 
+app.get('/ping', function(req, res) {
+    res.send({
+        alive: true
+    });
+});
+
 var port = Number(process.env.PORT || 5000);
 
 app.listen(port, function() {
